35da3385d800f52253c480df77ea2a39c3e80d4a
Olga Scrivner <obscrivn@iu.edu>
2025-09-04 12:54:40 -0400

    test

---
+++ labs/Test.java
@@ -0,0 +1,18 @@
+package practice1;
+
+/**
+ * Testing file
+ */
+public class Test {
+
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
