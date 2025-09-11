546fa3baad98e8acb14f207def0c72ab75647603
Olga Scrivner <obscrivn@iu.edu>
2025-09-11 09:53:27 -0400

    lab04

---
+++ labs/PhoneBook.java
@@ -0,0 +1,42 @@
+package myscanner;
+/**
+ * @author your name
+ * A program that stores phone numbers
+ * Phonebook entries:
+ * <br>
+ * 
+Alice → 555-1234<br>
+Bob → 555-9876<br>
+Charlie → 555-2468
+<br>
+Enter a name: Bob<br>
+Phone number: 555-9876
+
+<br>
+Enter a name: Eve<br>
+Not found.
+ */
+public class PhoneBook {
+
+	public static void main(String[] args) {
+		// TODO: create hashmap key -> value
+		
+		// TODO: add 3 entries
+		
+		System.out.println("Phonebook entries:");
+	
+		// TODO: create a for loop and print key -> value pairs
+		
+		//TODO: create scanner
+		
+		System.out.print("\nEnter a name: ");
+		
+		//TODO: create a string and store the name
+		
+		// TODO: Lookup and print result
+		
+		// close scanner
+
+	}
+
+}
