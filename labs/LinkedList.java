5a99aa8b3505459d663a9c49a94218625d3ecfd0
Olga Scrivner <obscrivn@iu.edu>
2026-05-13 00:12:42 -0400

    w20

--- labs/LinkedList.java
+++ labs/LinkedList.java
@@ -1,275 +1,134 @@
+package linked01;
 
+
+/**
+ * LinkedList object
+ * Stores:
+ * - head → reference to the first node
+ *
+ * Node object
+ * Stores:
+ * - value → data inside the node
+ * - next → reference to the next node
+ */
 public class LinkedList {
+	// head points to the first node in the list.
+    // If head is null, the list is empty.
+	private Node head;
 	
-	
-	private class Node {
-		public int value;
-		public Node next;
+	public LinkedList() {
+		// TODO 1: Start with a null head because the list is empty.
 		
+	}
+	
+
+	
+	public void addAtBeginning(int value) {
+		
+		// TODO 2: Create a new node. The new node stores value and points to the OLD head.
+		
+		
+		// TODO 3: Move head so it points to the NEW first node.
+        
+
+    }
+	
+	/**
+	 * Counts how many nodes exist in the list.
+	 */
+	public int size() {
+
+	    // TODO 12 calculate the list size: 
+		
+		// Start count at 0.
+		
+	    // Start current at the FIRST node.
+	  
+	    // (begin while loop) Keep walking until current becomes null.
+	   
+	        // Count this node.
+	      
+	        // Move current to the next node.
+	   // end while loop
+		// Return the total number of nodes.
+	    return 0;
+	}
+	
+	public String toString() {
+		// Start at the head.
+	    // head always stores where the list begins.
+        Node current = this.head;
+        String result ="head->";
+     // Keep walking through the list
+        // until current node reaches null (the end).
+        while(current!=null) {
+        // Add the current node's value.
+          result+= current.value;
+          result+="->";
+          // Move current forward to the next node.
+          current=current.next;
+        }
+        // The last node points to null.
+        result+="null";
+        return result;
+      }
+	
+	/**
+	 * Removes the FIRST node from the list.
+	 */
+	public void deleteFirst() {
+
+	    // TODO 8: Check if the list is already empty.
+
+
+	    // TODO 9: Move head to the next node.
+	    // The old first node is removed from the list.
+
+	}
+	
+
+	/**
+	 * Node class stores:
+	 * - value → data stored in this node
+	 * - next → reference to ANOTHER Node object
+	 *
+	 * Why is next type Node?
+	 * Because each node connects to another node in the chain.
+	 */
+	private class Node {
+		int value;
+		Node next;
+
 		public Node(int value, Node next) {
 			this.value = value;
 			this.next = next;
 		}
 	}
 	
-	private Node head;
-	
-	public LinkedList() {
-		head = null;
-	}
-	
-	
-	/**
-	 * 
-	 * Adds a value at the beginning of the list
-	 * 
-	 * @param value
-	 */
-	public void addAtBeginning(int value) {
-		this.head = new Node(value,this.head);
-	}
-	
-	/**
-	 * Converts the list to a string like this:
-	 * 1->2->3->null
-	 */
-	public String toString() {
-		Node current = this.head;
-		String result = "";
-		while(current != null) {
-			result += current.value;
-			result += "->";
-			current = current.next;
-		}
-		result += "null";
-		return result;
-	}
-	
-	/**
-	 * 
-	 * Example of how the linked list is used
-	 * 
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		LinkedList list = new LinkedList();
-		list.addAtBeginning(1);
-		list.addAtBeginning(2);
-		list.addAtBeginning(3);
-		System.out.println(list.toString());
+		// TODO 4: Create a new empty LinkedList.
+	
 		
-	}
+		// TODO 5: Add 3 at the beginning.
 	
-	/**
-	 * Returns the number of elements in the linked list
-	 * 
-	 * The code for this will look a little bit like toString
-	 * 
-	 * @return
-	 */
-	public int size() {
-		int count = 0;
-		Node current = this.head;
-		while (current != null){
-			count++;
-			current = current.next;
-		}
-		return count;
-	}
-	
-	
-	
-	/**
-	 * 
-	 * Add value at the end of the linked list
-	 * 
-	 * To solve this problem you'll need a loop
-	 * to find the last element of the list.
-	 * 
-	 * You'll also need a special case for the
-	 * empty list.
-	 * 
-	 * @param value
-	 */
-	public void addAtEnd(int value) {
-		if(this.head == null) {
-			addAtBeginning(value);
-			return;
-		}
-		Node current = this.head;
-		while(current.next != null) {
-			current = current.next;
-		}
-		current.next = new Node(value, null);
-	}
-	
-	/**
-	 * 
-	 * Inserts a particular value after another value in the list
-	 * 
-	 * For example, in the list
-	 * 
-	 * 1->2->3->null
-	 * 
-	 * Adding the value 77 after the 2 would give
-	 * 
-	 * 1->2->77->3->null
-	 * 
-	 * You can assume the value will definitely be in the list
-	 * 
-	 * @param valueToAdd
-	 * @param valueToInsertAfter
-	 */
-	public void addAfterValue(int valueToAdd, int valueToInsertAfter) {
-		Node current = this.head;
-		while(current.value != valueToInsertAfter) {
-			current = current.next;
-		}
-		current.next = new Node(valueToAdd,current.next);
-	}
-	
-	/**
-	 * 
-	 * Inserts a particular value before another value in the list
-	 * 
-	 * For example, in the list
-	 * 
-	 * 1->2->3->null
-	 * 
-	 * Adding the value 77 before the 2 would give
-	 * 
-	 * 1->77->2->3->null
-	 * 
-	 * You can assume the value will definitely be in the list
-	 * 
-	 * To solve this, you'll need to keep track of both a current
-	 * and previous node as you go through the list.
-	 * 
-	 * You'll also need a special case for the head of the list.
-	 * 
-	 * @param valueToAdd
-	 * @param valueToInsertBefore
-	 */
-	public void addBeforeValue(int valueToAdd, int valueToInsertBefore) {
-		if(this.head.value == valueToInsertBefore) {
-			addAtBeginning(valueToAdd);
-			return;
-		}
-		Node prev = this.head;
-		Node current = this.head.next;
-		while(current.value != valueToInsertBefore) {
-			current = current.next;
-			prev = prev.next;
-		}
-		prev.next = new Node(valueToAdd,current);
-	
-	}
-	
-	/**
-	 * 
-	 * This constructor takes and array and makes a linked list
-	 * containing all the elements in the array.
-	 * 
-	 * You could do this by repeatedly calling add at end
-	 * but be classy and construct the list by hand.
-	 * 
-	 * You can assume the array has at least one element.
-	 * 
-	 * @param data
-	 */
-	public LinkedList(int[] data) {
-		this.head = new Node(data[0],null);
-		Node current = this.head;
-		for(int i = 1; i < data.length; i++) {
-			current.next = new Node(data[i],null);
-			current = current.next;
-		}
-	}
-	
-	/**
-	 * Removes the first element from the list
-	 * 
-	 * If the list is empty, throw an exception like this:
-	 * throw new RuntimeException("Attempt to remove from an empty list");
-	 */
-	public void removeBeginning() {
-		if(head == null)
-			throw new RuntimeException("Attempt to remove from an empty list");
-		this.head = this.head.next;
-	}
-	
-	/**
-	 * Removes the last element from the list
-	 * 
-	 * You'll need a special case for the 1 element list
-	 * 
-	 * If the list is empty, throw an exception like this:
-	 * throw new RuntimeException("Attempt to remove from an empty list");
-	 */
-	public void removeEnd() {
-		if(head == null)
-			throw new RuntimeException("Attempt to remove from an empty list");
-		if(head.next == null) {
-			head = null;
-			return;
-		}
-		Node current = head;
-		while(current.next.next != null) {
-			current = current.next;
-		}
-		current.next = null;
-	}
-	
-	/**
-	 * Removes the element after a particular value in the list
-	 * 
-	 * For example, given this this list:
-	 * 1->2->3->null
-	 * 
-	 * Removing after the element 1 would result in:
-	 * 1->3->null
-	 * 
-	 * 
-	 * You can assume that both the element and the element after
-	 * exist in the list.
-	 * 
-	 * @param valueToRemoveAfter
-	 */
-	public void removeAfter(int valueToRemoveAfter) {
-		Node current = this.head;
-		while(current.value != valueToRemoveAfter) {
-			current = current.next;
-		}
-		current.next = current.next.next;
-	}
-	
-	/**
-	 * 
-	 * Removes all the nodes with a particular value from the list
-	 * 
-	 * For example, given the list
-	 * 
-	 * 1->2->3->2->null
-	 * 
-	 * Removing all the 2s would result in:
-	 * 1->3->null
-	 * 
-	 * @param nodesWithValue
-	 */
-	public void removalAll(int nodesWithValue) {
-		while(this.head != null && this.head.value == nodesWithValue) {
-			removeBeginning();
-		}
-		if(this.head == null)
-			return;
-		Node current = head;
-		while(current.next != null) {
-			if(current.next.value == nodesWithValue) {
-				current.next = current.next.next;
-			} else {
-				current = current.next;
-			}
-		}
-	}	
+	    
+	    // TODO 6: Add 5 at the beginning.
 
-}
+	    
+	 // TODO 7: Print the list.
+		
+		
+		 // TODO 10: Delete the first node.
+
+	 // TODO 11: Print the list again.
+
+	    // TODO 13: Print the size.
+
+	}
+	
+}	
+	
+	
+	
+
+
